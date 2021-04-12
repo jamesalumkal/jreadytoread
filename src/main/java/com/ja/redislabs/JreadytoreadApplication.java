9dc3ed5f3ed6c0179294f080077cdc3ec02caf58
James Alumkal <james.alumkal@yahoo.com>
2021-04-12 09:32:20 -0700

    spring.io generated files

---
+++ src/main/java/com/ja/redislabs/JreadytoreadApplication.java
@@ -0,0 +1,13 @@
+package com.ja.redislabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JreadytoreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JreadytoreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ja/redislabs/JreadytoreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ja.redislabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JreadytoreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
